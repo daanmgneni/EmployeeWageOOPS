86035863fd43b6ee0498cb96661a163658e40531
daanmgneni <94113582+daanmgneni@users.noreply.github.com>
2023-03-18 23:00:59 +0530

    Update program.cs

--- program.cs
+++ program.cs
@@ -4,8 +4,9 @@ namespace EmployeeWagesProblemWithuseObj
     {
         public static void Main(string[] args) 
         {
+            //wages calculation for 20 days in month
             EmployeeWageCode obj = new EmployeeWageCode();
-            obj.EmployeeAttendence();
+            obj.WageCalculate();
         }
     }
 }
